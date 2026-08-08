85f79201e70fd271540ffd912d3958ad138028ed
stagas <gstagas@gmail.com>
2026-08-08 17:47:27 +0300

    chore: update css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -103,7 +103,7 @@ export function Layout({
             <link rel="alternate" type="application/atom+xml" title={`${feeds.title} (Atom)`} href={feeds.atom} />
           </>
         )}
-        <link rel="stylesheet" href="/styles.css?v=57" />
+        <link rel="stylesheet" href="/styles.css?v=58" />
       </head>
       <body>
         <a className="skip-link" href="#main-content">skip to content</a>
