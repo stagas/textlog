52eadbfc9e101c2949cf7a2569ebcba315dde0ee
stagas <gstagas@gmail.com>
2026-08-10 20:04:18 +0300

    fix: mobile css for color swatches

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -115,7 +115,7 @@ export function Layout({
             <link rel="alternate" type="application/atom+xml" title={`${feeds.title} (Atom)`} href={feeds.atom} />
           </>
         )}
-        <link rel="stylesheet" href="/styles.css?v=101" />
+        <link rel="stylesheet" href="/styles.css?v=102" />
         <link rel="stylesheet" href="/theme.css" />
       </head>
       <body>
