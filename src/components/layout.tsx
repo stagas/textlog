41c8db51d28debcba2fa2c52888e15c3d6a5aa49
stagas <gstagas@gmail.com>
2026-08-12 00:06:44 +0300

    style: tighten unread dot in for-you page with handle

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -123,7 +123,7 @@ export function Layout({
             <link rel="alternate" type="application/atom+xml" title={`${feeds.title} (Atom)`} href={feeds.atom} />
           </>
         )}
-        <link rel="stylesheet" href="/styles.css?v=148" />
+        <link rel="stylesheet" href="/styles.css?v=149" />
         <style>{themeCss}</style>
       </head>
       <body>
