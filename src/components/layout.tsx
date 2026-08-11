870d904da5fc70ccb4e11d4070ae6077ee04d838
stagas <gstagas@gmail.com>
2026-08-11 21:50:47 +0300

    chore: update css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -121,7 +121,7 @@ export function Layout({
             <link rel="alternate" type="application/atom+xml" title={`${feeds.title} (Atom)`} href={feeds.atom} />
           </>
         )}
-        <link rel="stylesheet" href="/styles.css?v=132" />
+        <link rel="stylesheet" href="/styles.css?v=133" />
         <style>{themeCss}</style>
       </head>
       <body>
