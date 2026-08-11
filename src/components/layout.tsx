c18588bd94940ae694bed2372a812bb355afbd44
stagas <gstagas@gmail.com>
2026-08-11 22:50:42 +0300

    fix: borders again

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -123,7 +123,7 @@ export function Layout({
             <link rel="alternate" type="application/atom+xml" title={`${feeds.title} (Atom)`} href={feeds.atom} />
           </>
         )}
-        <link rel="stylesheet" href="/styles.css?v=142" />
+        <link rel="stylesheet" href="/styles.css?v=143" />
         <style>{themeCss}</style>
       </head>
       <body>
