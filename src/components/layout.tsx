851feec49ced18078f9ec2038ff8be9bd37bb36c
stagas <gstagas@gmail.com>
2026-08-11 16:43:28 +0300

    refactor: use quote-ink for code

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -118,7 +118,7 @@ export function Layout({
             <link rel="alternate" type="application/atom+xml" title={`${feeds.title} (Atom)`} href={feeds.atom} />
           </>
         )}
-        <link rel="stylesheet" href="/styles.css?v=121" />
+        <link rel="stylesheet" href="/styles.css?v=122" />
         <style>{themeCss}</style>
       </head>
       <body>
