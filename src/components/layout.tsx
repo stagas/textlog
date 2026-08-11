452107df1daed38616d3a693a40ebfb792ab6c78
stagas <gstagas@gmail.com>
2026-08-11 20:47:52 +0300

    fix: account/edit css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -121,7 +121,7 @@ export function Layout({
             <link rel="alternate" type="application/atom+xml" title={`${feeds.title} (Atom)`} href={feeds.atom} />
           </>
         )}
-        <link rel="stylesheet" href="/styles.css?v=130" />
+        <link rel="stylesheet" href="/styles.css?v=131" />
         <style>{themeCss}</style>
       </head>
       <body>
