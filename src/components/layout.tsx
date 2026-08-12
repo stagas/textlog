2fff47412b31a4838887453ef28f321424474477
stagas <gstagas@gmail.com>
2026-08-12 03:44:44 +0300

    style: reply form improvements

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -106,7 +106,7 @@ export function Layout({
             <link rel="alternate" type="application/atom+xml" title={`${feeds.title} (Atom)`} href={feeds.atom} />
           </>
         )}
-        <link rel="stylesheet" href="/styles.css?v=155" />
+        <link rel="stylesheet" href="/styles.css?v=156" />
         <style>{themeCss}</style>
       </head>
       <body>
