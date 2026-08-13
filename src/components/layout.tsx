42d817f04c4d4564e697b137c881246703b126a5
stagas <gstagas@gmail.com>
2026-08-13 16:08:20 +0300

    fix: underline geometry

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -122,7 +122,7 @@ export function Layout({
             <link rel="alternate" type="application/atom+xml" title={`${feeds.title} (Atom)`} href={feeds.atom} />
           </>
         )}
-        <link rel="stylesheet" href="/styles.css?v=225" />
+        <link rel="stylesheet" href="/styles.css?v=226" />
         <style>{themeCss}</style>
       </head>
       <body>
