584390369d18563775b8596e74e744352198302a
stagas <gstagas@gmail.com>
2026-08-13 13:48:12 +0300

    fix: tabs overflow horizontally in their own row

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -122,7 +122,7 @@ export function Layout({
             <link rel="alternate" type="application/atom+xml" title={`${feeds.title} (Atom)`} href={feeds.atom} />
           </>
         )}
-        <link rel="stylesheet" href="/styles.css?v=219" />
+        <link rel="stylesheet" href="/styles.css?v=220" />
         <style>{themeCss}</style>
       </head>
       <body>
