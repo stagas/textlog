13031a4ba087f1d765bcb8d0eb45e2b883378a37
stagas <gstagas@gmail.com>
2026-08-15 02:45:58 +0300

    chore: update css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -124,7 +124,7 @@ export function Layout({
             <link rel="alternate" type="application/atom+xml" title={`${feeds.title} (Atom)`} href={feeds.atom} />
           </>
         )}
-        <link rel="stylesheet" href="/styles.css?v=256" />
+        <link rel="stylesheet" href="/styles.css?v=257" />
         <style>{themeCss}</style>
       </head>
       <body className={`density-${density}`}>
