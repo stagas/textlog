8178a95f86917783b474357652d71d3d91a92564
stagas <gstagas@gmail.com>
2026-08-22 22:47:39 +0300

    style: helper popovers

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -160,7 +160,7 @@ export function Layout({
           </>
         )}
         {mobile && <link href="https://fonts.cdnfonts.com/css/dejavu-sans-mono" rel="stylesheet" />}
-        <link rel="stylesheet" href="/styles.css?v=567" />
+        <link rel="stylesheet" href="/styles.css?v=570" />
         <style>{themeCss}</style>
       </head>
       <body className={`density-${density}${user?.show_link_previews === 0 ? ' link-previews-disabled' : ''}${
