ab8dbb9138010d8520751c6d258204a8543951e7
stagas <gstagas@gmail.com>
2026-08-26 21:25:59 +0300

    chore: invalidate caches

--- src/materialized-feed-service.ts
+++ src/materialized-feed-service.ts
@@ -9,7 +9,7 @@ type MaterializedResponse = {
 }
 
 const materializations = new Map<string, Promise<MaterializedResponse>>()
-const MATERIALIZED_HTML_VERSION = 12
+const MATERIALIZED_HTML_VERSION = 13
 
 function appearanceVariant(request: Request) {
   const cookie = request.headers.get('cookie') || ''
