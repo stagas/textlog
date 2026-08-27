bfc81d139f6c1f7c16fdc92f40cfabe802cceda4
stagas <gstagas@gmail.com>
2026-08-27 12:48:58 +0300

    style: margins

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -166,7 +166,7 @@ export function Layout({
           </>
         )}
         {mobile && <link href="https://fonts.cdnfonts.com/css/dejavu-sans-mono" rel="stylesheet" />}
-        <link rel="stylesheet" href="/styles.css?v=860" />
+        <link rel="stylesheet" href="/styles.css?v=865" />
         <style>{themeCss}</style>
       </head>
       <body
