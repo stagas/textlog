8866976182d9861b821dcdec4ef63458df70348b
stagas <gstagas@gmail.com>
2026-08-27 12:38:40 +0300

    style: consent margins

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -166,7 +166,7 @@ export function Layout({
           </>
         )}
         {mobile && <link href="https://fonts.cdnfonts.com/css/dejavu-sans-mono" rel="stylesheet" />}
-        <link rel="stylesheet" href="/styles.css?v=855" />
+        <link rel="stylesheet" href="/styles.css?v=860" />
         <style>{themeCss}</style>
       </head>
       <body
