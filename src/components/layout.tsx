7f4cfc6b33182821606a7f4253354926c1497e57
stagas <gstagas@gmail.com>
2026-08-28 09:17:48 +0300

    style: exec result

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -171,7 +171,7 @@ export function Layout({
           </>
         )}
         {mobile && <link href="https://fonts.cdnfonts.com/css/dejavu-sans-mono" rel="stylesheet" />}
-        <link rel="stylesheet" href="/styles.css?v=920" />
+        <link rel="stylesheet" href="/styles.css?v=922" />
         <style>{themeCss}</style>
       </head>
       <body
