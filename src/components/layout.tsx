c08e12835dfd348f00902fb831d3b70e221ad679
stagas <gstagas@gmail.com>
2026-08-30 21:09:33 +0300

    fix: table corners on rounded

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -174,7 +174,7 @@ export function Layout({
           </>
         )}
         {mobile && <link href="https://fonts.cdnfonts.com/css/dejavu-sans-mono" rel="stylesheet" />}
-        <link rel="stylesheet" href="/styles.css?v=1171" />
+        <link rel="stylesheet" href="/styles.css?v=1172" />
         <style>{themeCss}</style>
       </head>
       <body
