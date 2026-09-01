000032ed7525627d0d867296552c8416141dc839
stagas <gstagas@gmail.com>
2026-09-01 19:51:53 +0300

    enhancement: enlarge emoji on handle hover

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -184,7 +184,7 @@ export function Layout({
           </>
         )}
         {mobile && <link href="https://fonts.cdnfonts.com/css/dejavu-sans-mono" rel="stylesheet" />}
-        <link rel="stylesheet" href="/styles.css?v=1229" />
+        <link rel="stylesheet" href="/styles.css?v=1232" />
         <style>{themeCss}</style>
       </head>
       <body
