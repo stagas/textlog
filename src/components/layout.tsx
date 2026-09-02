3dabbfb5f1560f01ca66b3a500f7584e478e657a
stagas <gstagas@gmail.com>
2026-09-02 17:59:20 +0300

    style: footer links distance

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -184,7 +184,7 @@ export function Layout({
           </>
         )}
         {mobile && <link href="https://fonts.cdnfonts.com/css/dejavu-sans-mono" rel="stylesheet" />}
-        <link rel="stylesheet" href="/styles.css?v=1314" />
+        <link rel="stylesheet" href="/styles.css?v=1315" />
         <style>{themeCss}</style>
       </head>
       <body
