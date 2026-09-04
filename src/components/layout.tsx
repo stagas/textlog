36fb3818f47476624370987209bfa2e6c83737fe
stagas <gstagas@gmail.com>
2026-09-04 13:36:09 +0300

    fix: logs height

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -205,7 +205,7 @@ export function Layout({
           </>
         )}
         {mobile && <link href="https://fonts.cdnfonts.com/css/dejavu-sans-mono" rel="stylesheet" />}
-        <link rel="stylesheet" href="/styles.css?v=1401" />
+        <link rel="stylesheet" href="/styles.css?v=1402" />
         <style>{themeCss}</style>
       </head>
       <body
