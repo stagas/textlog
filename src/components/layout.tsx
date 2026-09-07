51066ccdce696517385d0775bf37b1f9d7a17193
stagas <gstagas@gmail.com>
2026-09-07 14:16:10 +0300

    style: admin actions bottom row

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -207,7 +207,7 @@ export function Layout({
           </>
         )}
         {mobile && <link href="https://fonts.cdnfonts.com/css/dejavu-sans-mono" rel="stylesheet" />}
-        <link rel="stylesheet" href="/styles.css?v=1493" />
+        <link rel="stylesheet" href="/styles.css?v=1495" />
         <style dangerouslySetInnerHTML={{ __html: themeCss }} />
       </head>
       <body
